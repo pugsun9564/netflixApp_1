1faffa6adcf1ca748a8cc902a0bfd13f920e9c99
SunWoong <lovering1771@naver.com>
2023-06-21 18:28:11 +0900

    navigationBar

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "54911f43d0f3c86adebfc4f67e1b3e66",
+    language: "kp-KR",
+  },
+});
+
+export default instance;
