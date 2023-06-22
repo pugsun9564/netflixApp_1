497138d1695c2d0fb9ee467d3b49bd89e25b504b
SunWoong <lovering1771@naver.com>
2023-06-22 16:58:18 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
+import requests from "./api/requests";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import Row from "./components/Row";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Treanding Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Footer />
     </div>
   );
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: "54911f43d0f3c86adebfc4f67e1b3e66",
-    language: "kp-KR",
+    language: "ko-KR",
   },
 });
 
